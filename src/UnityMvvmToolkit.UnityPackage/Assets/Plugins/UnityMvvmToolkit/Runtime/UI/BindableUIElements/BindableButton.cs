179e4be9634c6bc3ecfd932b1a59b993f8abfe64
ChebanovDD <chebanovdd@gmail.com>
2022-08-10 13:37:55 +0700

    Add Enabled attribute to BindableButton.

--- src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/UI/BindableUIElements/BindableButton.cs
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/UI/BindableUIElements/BindableButton.cs
@@ -5,6 +5,12 @@ namespace UnityMvvmToolkit.UI.BindableUIElements
 {
     public class BindableButton : Button, IBindableUIElement
     {
+        public bool Enabled
+        {
+            get => enabledSelf;
+            set => SetEnabled(value);
+        }
+
         public string Command { get; set; }
 
         public new class UxmlFactory : UxmlFactory<BindableButton, UxmlTraits>
@@ -13,13 +19,19 @@ namespace UnityMvvmToolkit.UI.BindableUIElements
 
         public new class UxmlTraits : Button.UxmlTraits
         {
+            private readonly UxmlBoolAttributeDescription _enabledAttribute = new()
+                { name = "enabled", defaultValue = true };
+
             private readonly UxmlStringAttributeDescription _commandAttribute = new()
                 { name = "command", defaultValue = "" };
 
             public override void Init(VisualElement visualElement, IUxmlAttributes bag, CreationContext context)
             {
                 base.Init(visualElement, bag, context);
-                ((BindableButton) visualElement).Command = _commandAttribute.GetValueFromBag(bag, context);
+
+                var bindableButton = (BindableButton) visualElement;
+                bindableButton.Enabled = _enabledAttribute.GetValueFromBag(bag, context);
+                bindableButton.Command = _commandAttribute.GetValueFromBag(bag, context);
             }
         }
     }
