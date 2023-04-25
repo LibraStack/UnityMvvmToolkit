68de030ee1a4f3ebc4cdc6e50a2adf8c3df6074b
ChebanovDD <chebanovdd@gmail.com>
2023-04-25 13:20:04 +0800

    Fix TaskScrollView style.

--- samples/Unity.Mvvm.ToDoList/Assets/Scripts/BindableUIElements/BindableTaskScrollView.cs
+++ samples/Unity.Mvvm.ToDoList/Assets/Scripts/BindableUIElements/BindableTaskScrollView.cs
@@ -1,9 +1,17 @@
-﻿using UnityMvvmToolkit.UITK.BindableUIElements;
+﻿using UnityEngine.UIElements;
+using UnityMvvmToolkit.UITK.BindableUIElements;
 using ViewModels;
 
 namespace BindableUIElements
 {
     public partial class BindableTaskScrollView : BindableScrollView<TaskItemViewModel>
     {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            contentViewport.style.overflow = Overflow.Visible;
+            contentContainer.style.overflow = Overflow.Visible;
+        }
     }
 }
