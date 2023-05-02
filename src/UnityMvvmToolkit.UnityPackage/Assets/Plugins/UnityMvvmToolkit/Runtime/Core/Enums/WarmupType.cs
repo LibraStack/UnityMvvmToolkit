39b48ebe5d8a44ecb00f243fc9bc2a9299a7b605
ChebanovDD <chebanovdd@gmail.com>
2023-05-02 13:27:21 +0800

    Add WarmupType comments.

--- src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/Core/Enums/WarmupType.cs
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/Core/Enums/WarmupType.cs
@@ -2,8 +2,21 @@
 {
     public enum WarmupType
     {
+        /// <summary>
+        /// Use this type if you don't specify a converter in the view.
+        /// <example>command="PrintIntParameterCommand, 5"</example>
+        /// </summary>
         OnlyByType,
+
+        /// <summary>
+        /// Use this type if you specify a converter in the view.
+        /// <example>command="PrintIntParameterCommand, 5, ParameterToIntConverter"</example>
+        /// </summary>
         OnlyByName,
+
+        /// <summary>
+        /// Use this type to cover <see cref="OnlyByType"/> and <see cref="OnlyByName"/> scenarios.
+        /// </summary>
         ByTypeAndName
     }
 }
