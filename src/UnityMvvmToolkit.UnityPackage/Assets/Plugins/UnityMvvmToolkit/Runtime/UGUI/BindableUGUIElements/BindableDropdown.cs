e3de7566fc0b64c2876172f66308bf82512bbd55
Vasilev <35258579+VasilevMaxim@users.noreply.github.com>
2023-06-26 05:25:04 +0300

    Bindable dropdown (#31)
    Add a bindable Dropdown element.

---
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/UGUI/BindableUGUIElements/BindableDropdown.cs
@@ -0,0 +1,127 @@
+﻿#if UNITYMVVMTOOLKIT_TEXTMESHPRO_SUPPORT
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityMvvmToolkit.Core;
+using UnityMvvmToolkit.Core.Extensions;
+using UnityMvvmToolkit.Core.Interfaces;
+
+namespace UnityMvvmToolkit.UGUI.BindableUGUIElements
+{
+    [RequireComponent(typeof(TMP_Dropdown))]
+    public class BindableDropdown : MonoBehaviour, IBindableElement
+    {
+        [SerializeField] private TMP_Dropdown _dropdown;
+        [SerializeField] private string _bindingSelectedItemPath;
+        [SerializeField] private string _bindingItemsSourcePath;
+
+        private IProperty<string> _selectedItemProperty;
+        private IReadOnlyProperty<ObservableCollection<string>> _itemsSource;
+
+        private PropertyBindingData _selectedItemBindingData;
+        private PropertyBindingData _itemsSourceBindingData;
+        
+        public void SetBindingContext(IBindingContext context, IObjectProvider objectProvider)
+        {
+            if (string.IsNullOrWhiteSpace(_bindingItemsSourcePath) == false)
+            {
+                _itemsSourceBindingData ??= _bindingItemsSourcePath.ToPropertyBindingData();
+                _itemsSource = objectProvider
+                    .RentReadOnlyProperty<ObservableCollection<string>>(context, _itemsSourceBindingData);
+                _itemsSource.Value.CollectionChanged += OnItemsCollectionChanged;
+                _dropdown.options = new List<TMP_Dropdown.OptionData>(_itemsSource.Value.Select(value => new TMP_Dropdown.OptionData(value)));
+            }
+            
+            if (string.IsNullOrWhiteSpace(_bindingSelectedItemPath) == false)
+            {
+                _selectedItemBindingData ??= _bindingSelectedItemPath.ToPropertyBindingData();
+                _selectedItemProperty = objectProvider.RentProperty<string>(context, _selectedItemBindingData);
+                _selectedItemProperty.ValueChanged += OnPropertySelectedItemChanged;
+                
+                var foundIndex = _dropdown.options.FindIndex(option => option.text == _selectedItemProperty.Value);
+                if (foundIndex != -1)
+                {
+                    UpdateControlValue(foundIndex);
+                }
+                
+                _dropdown.onValueChanged.AddListener(OnControlValueChanged);
+                _selectedItemProperty.Value = _dropdown.options.Count > 0 ? _dropdown.options[0].text : default;
+            }
+        }
+
+        private void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (string newItem in e.NewItems)
+                {
+                    _dropdown.options.Add(new TMP_Dropdown.OptionData(newItem));
+                }
+            }
+
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (string oldItem in e.OldItems)
+                {
+                    _dropdown.options.Remove(new TMP_Dropdown.OptionData(oldItem));
+                }
+            }
+            
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                _dropdown.options.Clear();
+            }
+        }
+        
+        public virtual void ResetBindingContext(IObjectProvider objectProvider)
+        {
+            if (_itemsSource != null)
+            {
+                _itemsSource.Value.CollectionChanged -= OnItemsCollectionChanged;
+                objectProvider.ReturnReadOnlyProperty(_itemsSource);
+                _itemsSource = null;
+                _dropdown.options = new List<TMP_Dropdown.OptionData>();
+            }
+            
+            if (_selectedItemProperty != null)
+            {
+                _selectedItemProperty.ValueChanged -= OnPropertySelectedItemChanged;
+                objectProvider.ReturnProperty(_selectedItemProperty);
+                _selectedItemProperty = null;
+                _dropdown.onValueChanged.RemoveListener(OnControlValueChanged);
+            }
+            
+            UpdateControlValue(default);
+        }
+
+        protected virtual void OnControlValueChanged(int index)
+        {
+            _selectedItemProperty.Value = _dropdown.options[index].text;
+        }
+
+        private void OnPropertySelectedItemChanged(object sender, string newValue)
+        {
+            var foundIndex = _dropdown.options.FindIndex(option => option.text == newValue);
+            if (foundIndex == -1)
+            {
+                return;
+            }
+            
+            UpdateControlValue(foundIndex);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected virtual void UpdateControlValue(int newValue)
+        {
+            _dropdown.SetValueWithoutNotify(newValue);
+        }
+    }
+}
+
+#endif
---
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/UITK/BindableUIElements/Uxmls/BindableDropdownField.Uxml.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.UIElements;
+using UnityMvvmToolkit.UITK.Extensions;
+
+namespace UnityMvvmToolkit.UITK.BindableUIElements
+{
+    partial class BindableDropdownField
+    {
+        public string BindingSelectedItemPath { get; private set; }
+
+        public string BindingItemsSourcePath { get; private set; }
+
+        public new class UxmlFactory : UxmlFactory<BindableDropdownField, UxmlTraits>
+        {
+        }
+        
+#if UNITY_2023_2_OR_NEWER
+        [System.Serializable]
+        public new class UxmlSerializedData : DropdownField.UxmlSerializedData
+        {
+            // ReSharper disable once InconsistentNaming
+            #pragma warning disable 649
+            [UnityEngine.SerializeField] private string BindingSelectedItemPath;
+            [UnityEngine.SerializeField] private string BindingItemsSourcePath;
+            #pragma warning restore 649
+
+            public override object CreateInstance() => new BindableDropdownField();
+            public override void Deserialize(object visualElement)
+            {
+                base.Deserialize(visualElement);
+
+                var bindableDropdownField = visualElement.As<BindableDropdownField>();
+                bindableDropdownField.BindingSelectedItemPath = BindingSelectedItemPath;
+                bindableDropdownField.BindingItemsSourcePath = BindingItemsSourcePath;
+            }
+        }
+#else
+        public new class UxmlTraits : DropdownField.UxmlTraits
+        {
+            private readonly UxmlStringAttributeDescription _bindingSelectedItemPath = new()
+                { name = "binding-selected-item-path", defaultValue = "" };
+
+            private readonly UxmlStringAttributeDescription _bindingItemsSourcePath = new()
+                { name = "binding-items-source-path", defaultValue = "" };
+
+            public override void Init(VisualElement visualElement, IUxmlAttributes bag, CreationContext context)
+            {
+                base.Init(visualElement, bag, context);
+
+                var bindableDropdownField = visualElement.As<BindableDropdownField>();
+                
+                bindableDropdownField.BindingSelectedItemPath = _bindingSelectedItemPath.GetValueFromBag(bag, context);
+                bindableDropdownField.BindingItemsSourcePath = _bindingItemsSourcePath.GetValueFromBag(bag, context);
+            }
+        }
+#endif
+    }
+}
