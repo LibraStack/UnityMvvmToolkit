4fb9f200b61e4e85ff0dc596f60ae368ebbc564a
ChebanovDD <chebanovdd@gmail.com>
2023-07-07 18:38:50 +0800

    Add TItem and TCollection BindableListView.

---
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/UITK/BindableUIElements/BindableListView.TItem.TCollection.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine.UIElements;
+using UnityMvvmToolkit.Common.Interfaces;
+using UnityMvvmToolkit.Core;
+using UnityMvvmToolkit.Core.Extensions;
+using UnityMvvmToolkit.Core.Interfaces;
+using UnityMvvmToolkit.UITK.Extensions;
+
+namespace UnityMvvmToolkit.UITK.BindableUIElements
+{
+    public abstract partial class BindableListView<TItemBindingContext, TCollection> : 
+        ListView, IBindableCollection, IInitializable, IDisposable
+        where TItemBindingContext : ICollectionItem
+        where TCollection : IList<TItemBindingContext>, IList, INotifyCollectionChanged
+    {
+        private VisualTreeAsset _itemTemplate;
+
+        private IObjectProvider _objectProvider;
+        private List<VisualElement> _itemAssets;
+        private IList<TItemBindingContext> _itemsSource;
+        private Dictionary<int, TItemBindingContext> _activeItems;
+
+        protected PropertyBindingData _itemsSourceBindingData;
+        protected IReadOnlyProperty<TCollection> _itemsSourceProperty;
+
+        public virtual void Initialize()
+        {
+            _itemAssets = new List<VisualElement>();
+            _activeItems = new Dictionary<int, TItemBindingContext>();
+        }
+
+        public virtual void Dispose()
+        {
+            for (var i = 0; i < _itemAssets.Count; i++)
+            {
+                _itemAssets[i].DisposeBindableElement(_objectProvider);
+            }
+
+            _itemAssets.Clear();
+            _activeItems.Clear();
+        }
+
+        public virtual void SetBindingContext(IBindingContext context, IObjectProvider objectProvider)
+        {
+            if (string.IsNullOrWhiteSpace(BindingItemsSourcePath))
+            {
+                return;
+            }
+
+            _itemsSourceBindingData ??= BindingItemsSourcePath.ToPropertyBindingData();
+            _itemTemplate ??= objectProvider.GetCollectionItemTemplate<TItemBindingContext, VisualTreeAsset>();
+
+            _objectProvider = objectProvider;
+
+            _itemsSourceProperty = objectProvider.RentReadOnlyProperty<TCollection>(context, _itemsSourceBindingData);
+            _itemsSourceProperty.Value.CollectionChanged += OnItemsCollectionChanged;
+            _itemsSource = _itemsSourceProperty.Value;
+
+            itemsSource = _itemsSourceProperty.Value;
+            makeItem += OnMakeItem;
+            bindItem += OnBindItem;
+            unbindItem += OnUnbindItem;
+        }
+
+        public virtual void ResetBindingContext(IObjectProvider objectProvider)
+        {
+            if (_itemsSourceProperty is null)
+            {
+                return;
+            }
+
+            _itemsSourceProperty.Value.CollectionChanged -= OnItemsCollectionChanged;
+
+            makeItem -= OnMakeItem;
+            bindItem -= OnBindItem;
+            unbindItem -= OnUnbindItem;
+            itemsSource = Array.Empty<TItemBindingContext>();
+
+            objectProvider.ReturnReadOnlyProperty(_itemsSourceProperty);
+
+            _itemTemplate = null;
+            _objectProvider = null;
+
+            _itemsSource = default;
+            _itemsSourceProperty = null;
+        }
+
+        protected virtual VisualElement MakeItem(VisualTreeAsset itemTemplate)
+        {
+            return itemTemplate
+                .InstantiateBindableElement()
+                .InitializeBindableElement();
+        }
+
+        protected virtual void BindItem(VisualElement item, int index, TItemBindingContext bindingContext,
+            IObjectProvider objectProvider)
+        {
+            item.SetChildsBindingContext(bindingContext, objectProvider);
+        }
+
+        protected virtual void UnbindItem(VisualElement item, int index, TItemBindingContext bindingContext,
+            IObjectProvider objectProvider)
+        {
+            item.ResetChildsBindingContext(objectProvider);
+        }
+
+        protected virtual void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+#if UNITY_2021
+            if (e.Action is NotifyCollectionChangedAction.Remove or NotifyCollectionChangedAction.Reset)
+            {
+                Rebuild();
+                return;
+            }
+#endif
+            RefreshItems();
+        }
+
+        private VisualElement OnMakeItem()
+        {
+            var item = MakeItem(_itemTemplate);
+
+            _itemAssets.Add(item);
+
+            return item;
+        }
+
+        private void OnBindItem(VisualElement item, int index)
+        {
+            var itemId = item.GetHashCode();
+            var itemBindingContext = _itemsSource[index];
+
+            if (_activeItems.TryAdd(itemId, itemBindingContext))
+            {
+                BindItem(item, index, itemBindingContext, _objectProvider);
+            }
+        }
+
+        private void OnUnbindItem(VisualElement item, int index)
+        {
+            var itemId = item.GetHashCode();
+
+            if (_activeItems.Remove(itemId, out var itemBindingContext))
+            {
+                UnbindItem(item, index, itemBindingContext, _objectProvider);
+            }
+        }
+    }
+}
---
+++ src/UnityMvvmToolkit.UnityPackage/Assets/Plugins/UnityMvvmToolkit/Runtime/UITK/BindableUIElements/Uxmls/BindableListView.TItem.TCollection.Uxml.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.UIElements;
+using UnityMvvmToolkit.UITK.Extensions;
+
+namespace UnityMvvmToolkit.UITK.BindableUIElements
+{
+    partial class BindableListView<TItemBindingContext, TCollection>
+    {
+        public string BindingItemsSourcePath { get; private set; }
+
+#if UNITY_2023_2_OR_NEWER
+        [System.Serializable]
+        public new class UxmlSerializedData : ListView.UxmlSerializedData
+        {
+            // ReSharper disable once InconsistentNaming
+            #pragma warning disable 649
+            [UnityEngine.SerializeField] private string BindingItemsSourcePath;
+            #pragma warning restore 649
+
+            public override void Deserialize(object visualElement)
+            {
+                base.Deserialize(visualElement);
+
+                visualElement
+                    .As<BindableListView<TItemBindingContext, TCollection>>()
+                    .BindingItemsSourcePath = BindingItemsSourcePath;
+            }
+        }
+#else
+        public new class UxmlTraits : ListView.UxmlTraits
+        {
+            private readonly UxmlStringAttributeDescription _bindingItemsSourceAttribute = new()
+                { name = "binding-items-source-path", defaultValue = "" };
+
+            public override void Init(VisualElement visualElement, IUxmlAttributes bag, CreationContext context)
+            {
+                base.Init(visualElement, bag, context);
+
+                visualElement
+                    .As<BindableListView<TItemBindingContext, TCollection>>()
+                    .BindingItemsSourcePath = _bindingItemsSourceAttribute.GetValueFromBag(bag, context);
+            }
+        }
+#endif
+    }
+}
